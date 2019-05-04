ce607ba1625ce90a45907af44bcc7e4e91f7162f
khantly101 <khantuanly@gmail.com>
2019-05-04 07:41:22 -0700

    test view change mobile

--- app.js
+++ app.js
@@ -836,3 +836,10 @@ $(()=>{
 
 })
 
+$(window).bind("orientationchange", function(){
+    var orientation = window.orientation;
+    var new_orientation = (orientation) ? 0 : 180 + orientation;
+    $('body').css({
+        "-webkit-transform": "rotate(" + new_orientation + "deg)"
+    });
+});
