3e6969c05bc85b7cf83081b32093d0399ae8ffb9
khantly101 <khantuanly@gmail.com>
2019-05-04 07:45:54 -0700

    remove test

--- app.js
+++ app.js
@@ -835,11 +835,3 @@ $(()=>{
 
 
 })
-
-$(window).bind("orientationchange", function(){
-    var orientation = window.orientation;
-    var new_orientation = (orientation) ? 0 : 180 + orientation;
-    $('body').css({
-        "-webkit-transform": "rotate(" + new_orientation + "deg)"
-    });
-});
